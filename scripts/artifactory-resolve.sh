a10967a7f415b5daadd4636d2a5f9396bd38e950
Tomas Ondrej <37583109+ondrejtomas@users.noreply.github.com>
2018-05-15 13:40:40 +0200

    Improve Travis CI (#12)

---
+++ scripts/artifactory-resolve.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+API_KEY=$1
+PATH_TO_FILE=$2
+TARGET_FILE_PATH=$3
+
+if [ -z "$3" ]; then
+    TARGET_FILE_PATH=$2
+fi
+
+DIR_URL=https://na.artifactory.swg-devops.com/artifactory/iot-waw-trevis-generic-local/${TRAVIS_BRANCH}/${TRAVIS_BUILD_NUMBER}
+
+curl -H 'X-JFrog-Art-Api: '${API_KEY} -O ${DIR_URL}/${PATH_TO_FILE}
+
+mkdir -p `dirname ${TARGET_FILE_PATH}`
+mv `basename ${PATH_TO_FILE}` ${TARGET_FILE_PATH}
+

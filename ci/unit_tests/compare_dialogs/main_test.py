c37b988f8311e4626c39dcda7a7752d060036e38
tondrej <tondrej@cz.ibm.com>
2019-03-11 17:01:54 +0100

    Improve code based on PR review

--- ci/unit_tests/compare_dialogs/main_test.py
+++ ci/unit_tests/compare_dialogs/main_test.py
@@ -1,10 +1,24 @@
+"""
+Copyright 2019 IBM Corporation
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 
 import os, pytest, json
 
 import compare_dialogs
 from .. import unit_utils
 
-class TestMain(unit_utils.TestCaseCapture):
+class TestMain(unit_utils.BaseTestCaseCapture):
 
     dataBasePath = './ci/unit_tests/compare_dialogs/compare_dialogs_data/'
 
@@ -23,14 +37,14 @@ class TestMain(unit_utils.TestCaseCapture):
     def callfunc(self, *args, **kwargs):
         compare_dialogs.main(*args, **kwargs)
 
-    ''' Tests some basic sets of args '''
     def test_args(self):
+        ''' Tests some basic sets of args '''
         self.tTooFewArgs([])
         self.tTooFewArgs(['/some/random/path'])
         self.tUnrecognizedArgs(['/some/random/path', '-s', 'randomNonPositionalArg'])
 
-    ''' Tests if the first file does not exist '''
     def test_nonExistentFileFirst(self):
+        ''' Tests if the first file does not exist '''
         self.tExitCodeAndErrMessage(
             1, # exit code
             'Input dialog json \'/some/random/path\' does not exist.', # error message substring
@@ -40,8 +54,8 @@ class TestMain(unit_utils.TestCaseCapture):
             ] # params (*args, **kwargs)
         )
 
-    ''' Tests if the second file does not exist '''
     def test_nonExistentFileSecond(self):
+        ''' Tests if the second file does not exist '''
         self.tExitCodeAndErrMessage(
             1, # exit code
             'Output dialog json \'/some/random/path\' does not exist.', # error message substring
@@ -51,8 +65,8 @@ class TestMain(unit_utils.TestCaseCapture):
             ] # params (*args, **kwargs)
         )
 
-    ''' Tests if the first file contains invalid json '''
     def test_invalidJsonFirst(self):
+        ''' Tests if the first file contains invalid json '''
         self.tRaiseError(
             ValueError, # exeption
             'No JSON object could be decoded', # error message substring
@@ -62,8 +76,8 @@ class TestMain(unit_utils.TestCaseCapture):
             ] # params (*args, **kwargs)
         )
 
-    ''' Tests if the second file contains invalid json '''
     def test_invalidJsonSecond(self):
+        ''' Tests if the second file contains invalid json '''
         self.tRaiseError(
             ValueError, # exeption
             'No JSON object could be decoded', # error message substring
@@ -73,78 +87,78 @@ class TestMain(unit_utils.TestCaseCapture):
             ] # params (*args, **kwargs)
         )
 
-    ''' Tests if basic jsons are same - null, null '''
     def test_compareBasicNullNull(self):
+        ''' Tests if basic jsons are same - null, null '''
         self.tExitCode(0, [self.nullJsonPath, self.nullJsonPath])
 
-    ''' Tests if basic jsons are same - value, value '''
     def test_compareBasicValueValue(self):
+        ''' Tests if basic jsons are same - value, value '''
         self.tExitCode(0, [self.valueJsonPath, self.valueJsonPath])
 
-    ''' Tests if basic jsons are same - empty dict, empty dict'''
     def test_compareBasicEmptyDictEmptyDict(self):
+        ''' Tests if basic jsons are same - empty dict, empty dict'''
         self.tExitCode(0, [self.emptyDictJsonPath, self.emptyDictJsonPath])
 
-    ''' Tests if basic jsons are same - empty list, empty list'''
     def test_compareBasicEmptyListEmptyList(self):
+        ''' Tests if basic jsons are same - empty list, empty list'''
         self.tExitCode(0, [self.emptyListJsonPath, self.emptyListJsonPath])
 
-    ''' Tests if basic jsons are different - null, value and value, null '''
     def test_compareBasicNullValue(self):
+        ''' Tests if basic jsons are different - null, value and value, null '''
         self.tExitCode(1, [self.nullJsonPath, self.valueJsonPath])
         self.tExitCode(1, [self.valueJsonPath, self.nullJsonPath])
 
-    ''' Tests if basic jsons are different - null, empty dict and empty dict, null '''
     def test_compareBasicNullEmptyDict(self):
+        ''' Tests if basic jsons are different - null, empty dict and empty dict, null '''
         self.tExitCode(1, [self.nullJsonPath, self.emptyDictJsonPath])
         self.tExitCode(1, [self.emptyDictJsonPath, self.nullJsonPath])
 
-    ''' Tests if basic jsons are different - null, empty list and empty list, null '''
     def test_compareBasicNullEmptyList(self):
+        ''' Tests if basic jsons are different - null, empty list and empty list, null '''
         self.tExitCode(1, [self.nullJsonPath, self.emptyListJsonPath])
         self.tExitCode(1, [self.emptyListJsonPath, self.nullJsonPath])
 
-    ''' Tests if basic jsons are different - value, empty dict and empty dict, value '''
     def test_compareBasicValueEmptyDict(self):
+        ''' Tests if basic jsons are different - value, empty dict and empty dict, value '''
         self.tExitCode(1, [self.valueJsonPath, self.emptyDictJsonPath])
         self.tExitCode(1, [self.emptyDictJsonPath, self.valueJsonPath])
 
-    ''' Tests if basic jsons are different - value, empty list and empty list, value '''
     def test_compareBasicValueEmptyList(self):
+        ''' Tests if basic jsons are different - value, empty list and empty list, value '''
         self.tExitCode(1, [self.valueJsonPath, self.emptyListJsonPath])
         self.tExitCode(1, [self.emptyListJsonPath, self.valueJsonPath])
 
-    ''' Tests if basic jsons are different - empty dict, empty list and empty list, empty dict '''
     def test_compareBasicEmptyDictEmptyList(self):
+        ''' Tests if basic jsons are different - empty dict, empty list and empty list, empty dict '''
         self.tExitCode(1, [self.emptyDictJsonPath, self.emptyListJsonPath])
         self.tExitCode(1, [self.emptyListJsonPath, self.emptyDictJsonPath])
 
-    ''' Tests if complex sorted jsons are same '''
     def test_compareComplexSorted(self):
+        ''' Tests if complex sorted jsons are same '''
         self.tExitCode(0, [self.complexDictJsonPath, self.complexDictJsonPath])
 
-    ''' Tests if complex unsorted jsons are same '''
     def test_compareComplexUnsorted(self):
+        ''' Tests if complex unsorted jsons are same '''
         self.tExitCode(0, [self.complexDictJsonPath, self.complexDictUnsortedJsonPath])
         self.tExitCode(0, [self.complexDictUnsortedJsonPath, self.complexDictJsonPath])
 
-    ''' Tests if complex jsons are different - empty ([] != {}) '''
     def test_compareComplexDifferentEmpty(self):
+        ''' Tests if complex jsons are different - empty ([] != {}) '''
         self.tExitCode(1, [self.complexDictJsonPath, self.complexDictDifferentEmptyJsonPath])
         self.tExitCode(1, [self.complexDictDifferentEmptyJsonPath, self.complexDictJsonPath])
 
-    ''' Tests if complex jsons are different - key '''
     def test_compareComplexDifferentKey(self):
+        ''' Tests if complex jsons are different - key '''
         self.tExitCode(1, [self.complexDictJsonPath, self.complexDictDifferentKeyJsonPath])
         self.tExitCode(1, [self.complexDictDifferentKeyJsonPath, self.complexDictJsonPath])
 
-    ''' Tests if complex jsons are different - value '''
     def test_compareComplexDifferentValue(self):
+        ''' Tests if complex jsons are different - value '''
         self.tExitCode(1, [self.complexDictJsonPath, self.complexDictDifferentValueJsonPath])
         self.tExitCode(1, [self.complexDictDifferentValueJsonPath, self.complexDictJsonPath])
 
-    ''' Tests if complex json and its subjson are different '''
     def test_compareComplexDifferentSub(self):
+        ''' Tests if complex json and its subjson are different '''
         self.tExitCode(1, [self.complexDictJsonPath, self.complexDictSubJsonPath])
         self.tExitCode(1, [self.complexDictSubJsonPath, self.complexDictJsonPath])
 
--- ci/unit_tests/unit_utils.py
+++ ci/unit_tests/unit_utils.py
@@ -1,22 +1,36 @@
+"""
+Copyright 2019 IBM Corporation
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 
 import os, pytest, unittest
 
-class TestCaseCapture(unittest.TestCase):
+class BaseTestCaseCapture(unittest.TestCase):
 
-    ''' Runs function with given arguments and tests if it fails and error message contains \'too few arguments\' '''
     def tTooFewArgs(self, *args, **kwargs):
+        ''' Runs function with given arguments and tests if it fails and error message contains \'too few arguments\' '''
         self.tExitCodeAndErrMessage(2, 'too few arguments', *args, **kwargs)
 
-    ''' Runs function with given arguments and tests if it fails and error message contains \'unrecognized arguments\' '''
     def tUnrecognizedArgs(self, *args, **kwargs):
+        ''' Runs function with given arguments and tests if it fails and error message contains \'unrecognized arguments\' '''
         self.tExitCodeAndErrMessage(2, 'unrecognized arguments', *args, **kwargs)
 
-    ''' Runs function with given arguments and tests exit code '''
     def tExitCode(self, exitCode, *args, **kwargs):
+        ''' Runs function with given arguments and tests exit code '''
         self.tExitCodeAndErrMessage(exitCode, '', *args, **kwargs)
 
-    ''' (Generic) Runs function with given arguments and tests exit code and error message '''
     def tExitCodeAndErrMessage(self, exitCode, errMessage, *args, **kwargs):
+        ''' (Generic) Runs function with given arguments and tests exit code and error message '''
         with pytest.raises(SystemExit) as pytest_wrapped_e:
             self.callfunc(*args, **kwargs)
         captured = self.capfd.readouterr()
@@ -24,16 +38,16 @@ class TestCaseCapture(unittest.TestCase):
         assert pytest_wrapped_e.value.code == exitCode
         assert errMessage in captured.err
 
-    ''' (Generic) Runs function with given arguments and tests exception '''
     def tRaiseError(self, errorType, errMessage, *args, **kwargs):
+        ''' (Generic) Runs function with given arguments and tests exception '''
         with pytest.raises(errorType) as pytest_wrapped_e:
             self.callfunc(*args, **kwargs)
         captured = self.capfd.readouterr()
         assert pytest_wrapped_e.type == errorType
         assert errMessage in pytest_wrapped_e.value
 
-    ''' (Need to be overidden) Function to be called and tested '''
     def callfunc(self, *args, **kwargs):
+        ''' (Need to be overidden) Function to be called and tested '''
         raise NotImplementedError()
 
     @pytest.fixture(autouse=True)

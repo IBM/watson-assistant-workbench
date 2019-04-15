a0499fbc1014747c21654df3c6a82e5c12aa6809
Alexej Popovič <48282021+AlexejPopovic@users.noreply.github.com>
2019-04-15 17:37:40 +0200

    I217 update xml schema (#320)
    * [#217] Add test for valid actions (json -> xml)
    
    * [#217] Add documentation for action in the xml schema
    
    * [#217] Add restrictions to action type in schema, update doc
    
    * [#217] Add tests for xml->json

---
+++ ci/unit_tests/dialog_json2xml/main_test.py
@@ -0,0 +1,56 @@
+"""
+Copyright 2019 IBM Corporation
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os, json
+
+import dialog_json2xml
+from ...test_utils import BaseTestCaseCapture
+
+class TestMain(BaseTestCaseCapture):
+
+    dataBasePath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'main_data')
+    testOutputPath = os.path.join(dataBasePath, 'outputs')
+
+
+    def setup_class(cls):
+        ''' Setup any state specific to the execution of the given class (which usually contains tests). '''
+        # create output folder
+        BaseTestCaseCapture.createFolder(TestMain.testOutputPath)
+
+    def callfunc(self, *args, **kwargs):
+        dialog_json2xml.main(*args, **kwargs)
+
+    def test_mainValidActions(self):
+        """Tests if the script successfully completes with valid input file with actions."""
+        inputJsonPath = os.path.abspath(os.path.join(self.dataBasePath, 'inputActionsValid.json'))
+        expectedXmlPath = os.path.abspath(os.path.join(self.dataBasePath, 'expectedActionsValid.xml'))
+
+        outputXmlDirPath = os.path.join(self.testOutputPath, 'outputActionsValidResult')
+        outputXmlPath = os.path.join(outputXmlDirPath, 'dialog.xml')
+
+        BaseTestCaseCapture.createFolder(outputXmlDirPath)
+
+        self.t_noException([[inputJsonPath, '-d', outputXmlDirPath]])
+
+        expectedXml = ""
+        outputXml = ""
+
+        with open(expectedXmlPath, 'r') as expectedXmlFile:
+            expectedXml = expectedXmlFile.read()
+        with open(outputXmlPath, 'r') as outputXmlFile:
+            outputXml = outputXmlFile.read()
+
+        assert expectedXml == outputXml
+
---
+++ ci/unit_tests/dialog_xml2json/main_test.py
@@ -0,0 +1,58 @@
+"""
+Copyright 2019 IBM Corporation
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os, json
+
+import dialog_xml2json
+from ...test_utils import BaseTestCaseCapture
+
+class TestMain(BaseTestCaseCapture):
+
+    dataBasePath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'main_data')
+    testOutputPath = os.path.join(dataBasePath, 'outputs')
+
+
+    def setup_class(cls):
+        ''' Setup any state specific to the execution of the given class (which usually contains tests). '''
+        # create output folder
+        BaseTestCaseCapture.createFolder(TestMain.testOutputPath)
+
+    def callfunc(self, *args, **kwargs):
+        dialog_xml2json.main(*args, **kwargs)
+
+    def test_mainValidActions(self):
+        """Tests if the script successfully completes with valid input file with actions."""
+        inputXmlPath = os.path.abspath(os.path.join(self.dataBasePath, 'inputActionsValid.xml'))
+        expectedJsonPath = os.path.abspath(os.path.join(self.dataBasePath, 'expectedActionsValid.json'))
+
+        outputJsonDirPath = os.path.join(self.testOutputPath, 'outputActionsValidResult')
+        outputJsonPath = os.path.join(outputJsonDirPath, 'dialog.json')
+
+        BaseTestCaseCapture.createFolder(outputJsonDirPath)
+
+        self.t_noException([['--common_dialog_main', inputXmlPath,
+                            '--common_outputs_dialogs', 'dialog.json',
+                            '--common_outputs_directory', outputJsonDirPath]])
+
+        expectedJson = ""
+        outputJson = ""
+
+        with open(expectedJsonPath, 'r') as expectedJsonFile:
+            expectedJson = expectedJsonFile.read()
+        with open(outputJsonPath, 'r') as outputJsonFile:
+            outputJson = outputJsonFile.read()
+
+        assert expectedJson == outputJson
+

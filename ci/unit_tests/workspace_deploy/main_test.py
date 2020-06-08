ac4700c82cfd8134d4d58c57a0d16887b7b1a987
Erez Bilgory <erezbi@il.ibm.com>
2020-06-08 12:34:21 +0300

    one more test fix

--- ci/unit_tests/workspace_deploy/main_test.py
+++ ci/unit_tests/workspace_deploy/main_test.py
@@ -140,7 +140,22 @@ class TestMain(BaseTestCaseCapture):
 
 
     def test_wrongCredentials(self):
-        """Tests if script errors while uploading workspace with wrong credentials."""
+        """Tests if script errors while uploading workspace with wrong apikey."""
+
+        jsonWorkspaceFilename = 'skill_with_name_and_description.json'
+
+        wrongParams = ['--common_outputs_directory', self.dataBasePath,
+                             '--common_outputs_workspace', jsonWorkspaceFilename,
+                             '--conversation_username', 'apikey',
+                             '--conversation_password', str(uuid.uuid4()),
+                             '--conversation_url', self.workspacesUrl,
+                             '--conversation_version', self.version,
+                             '-v']
+        self.t_exitCodeAndLogMessage(1, "Unauthorized", [wrongParams])
+
+
+    def test_badCredentials(self):
+        """Tests if script errors while uploading workspace with bad credentials."""
 
         jsonWorkspaceFilename = 'skill_with_name_and_description.json'
 
@@ -151,7 +166,7 @@ class TestMain(BaseTestCaseCapture):
                              '--conversation_url', self.workspacesUrl,
                              '--conversation_version', self.version,
                              '-v']
-        self.t_exitCodeAndLogMessage(1, "Unauthorized", [wrongParams])
+        self.t_exitCodeAndLogMessage(1, "Forbidden", [wrongParams])
 
 
     def test_args_basic(self):
